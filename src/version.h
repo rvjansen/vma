b9013e29197f65ff1a3964bcd218d9cc4e70f35d
Rene Vincent Jansen <rvjansen@xs4all.nl>
2021-02-20 13:08:57 +0100

    add 64 bit macos intel cli executable

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #define _VERSION_H
 #define LIBVER  "12.081a"
 #define VERSION wxT( "12.081a" )
-#define FILEVER 20,122,0,1
-#define PRODVER 20,122,0,1
+#define FILEVER 21,051,0,1
+#define PRODVER 21,051,0,1
 #define STRFILEVER "12.081a\0"
 #define STRPRODVER "12.081a\0"
 #endif
